865bdb487288a03e473ad4ecb28f5e107c6abb02
madiha-m <madiha.muhammad.dev@gmail.com>
2025-09-23 03:12:57 +0500

    feat(frontend:header): add Navbar components and header index export

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Container, NavbarActions, NavbarBrand, NavbarLinks } from '..'
 
 const Header = () => {
     return (
-        <header>Header</header>
+        <header>
+            <Container className='bg-red-300 max-w-full'>
+                {/* NavbarBrand */}
+                <NavbarBrand />
+
+                {/* NavbarLinks */}
+                <NavbarLinks />
+
+                {/* NavbarActions */}
+                <NavbarActions />
+            </Container>
+        </header>
     )
 }
 
---
+++ frontend/src/components/header/NavbarBrand.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NavbarBrand = () => {
+  return (
+    <div>NavbarBrand</div>
+  )
+}
+
+export default NavbarBrand
