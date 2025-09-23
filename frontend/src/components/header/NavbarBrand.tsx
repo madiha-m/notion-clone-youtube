4f316044dbe2639776892d98cf7fb325da2f8070
madiha-m <madiha.muhammad.dev@gmail.com>
2025-09-23 20:24:00 +0500

    feat(frontend:header): finalize NavbarBrand logo with dark green text and add hoverEffect utility in global styles

--- frontend/src/components/header/NavbarBrand.tsx
+++ frontend/src/components/header/NavbarBrand.tsx
@@ -1,8 +1,21 @@
+import { cn } from '@/lib/utils'
+import Image from 'next/image'
+import Link from 'next/link'
+
 import React from 'react'
 
-const NavbarBrand = () => {
+const NavbarBrand = ({ className }: { className?: string }) => {
   return (
-    <div>NavbarBrand</div>
+    <Link href='/'>
+      <h2
+        className={cn(
+          "text-2xl text-shop_dark_green font-black tracking-wider uppercase hover:text-shop_light_green hoverEffect group font-sans",
+          className
+        )}
+      >
+        Shopcar<span className='text-shop_light_green group-hover:text-shop_dark_green hoverEffect'>t</span>
+      </h2>
+    </Link >
   )
 }
 
