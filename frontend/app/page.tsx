a0eddea74c064b9e061a8bda6c46c6a6ca1b52c8
madiha-m <madiha.muhammad.dev@gmail.com>
2025-09-24 13:45:00 +0500

    feat(frontend): initialize Next.js app with ShadCN and Clerk setup

---
+++ frontend/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Let's build an AI productivity APP</h1>
+      <Button>Click Me</Button>
+    </div>
+  );
+}
