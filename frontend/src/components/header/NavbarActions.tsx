fa589e617245e270ab44cf0b9fd8c92990fa90a4
madiha-m <madiha.muhammad.dev@gmail.com>
2025-09-24 12:00:00 +0500

    Start new project

--- frontend/src/components/header/NavbarActions.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const NavbarActions = () => {
-  return (
-    <div>NavbarActions</div>
-  )
-}
-
-export default NavbarActions
--- frontend/src/components/header/index.ts
+++
@@ -1,5 +0,0 @@
-// components/header/index.ts
-export { default as Header } from "./Header";
-export { default as NavbarBrand } from "./NavbarBrand";
-export { default as NavbarActions } from "./NavbarActions";
-export { default as NavbarLinks } from "./NavbarLinks";
